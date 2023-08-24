ad282b6ea379dbfe37a1deaf4fb8951adf844cc2
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-08-24 02:09:44 -0400

    Add POST route to set cookies when user log in

--- express_server.js
+++ express_server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 //use EJS as its templating engine
 app.set("view engine", "ejs");
+
 //convert the request body into string
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -39,6 +42,8 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
+
 //Add a GET Route to Show the Form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -66,6 +71,8 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortUrl}`);
 });
 
+
+
 //Add POST Route to Delete URL resource
 app.post('/urls/:id/delete', (req, res) => {
   const {id} = req.params;
@@ -79,7 +86,20 @@ app.post('/urls/:id/edit', (req, res) => {
   const {id} = req.params; 
   urlDatabase[id]  = req.body.longURL;
   res.redirect(`/urls`)
-})
+});
+
+
+//Add POST Route to let user to Log in
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
