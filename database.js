294fcb9e0ef0a93ca6b4574301c48fb78aeeb884
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-08-28 00:36:21 -0400

    Add database

---
+++ database.js
@@ -0,0 +1,26 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+
+const users = {
+  user1RandomID: {
+    id: "user1RandomID",
+    email: "user@gmail.com",
+    password: "$2a$10$uBbNocseL78DMg8aeh0h/.vPvygCndai0hDnElT5BuOoO.0sY5bGy", // 123456
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "test@email.com",
+    password: "$2a$10$NzxTIzvhwPDR5dyV5Ap4m.qRTKq8lJzn9ll46FpBo5b/4a7FHNBeu", // password
+  }  
+};
+
+module.exports = { urlDatabase, users };
